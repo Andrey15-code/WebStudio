540767382f4d15588b6dd9371a339bd5e1d94ab9
TKACHENKO ANDRII <yyyyyr150t@gmail.com>
2026-03-01 15:27:39 +0200

    model window

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,16 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
+    closeModalBtns: document.querySelectorAll("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  refs.closeModalBtns.forEach((btn) => {
+    btn.addEventListener("click", toggleModal);
+  });
+
   refs.modal.addEventListener("click", (event) => {
     if (event.target === refs.modal) {
       toggleModal();
